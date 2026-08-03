399b743356af4de6fe771e8ddc76ad53cea3ba90
Darshan P <225718512+darshanpandurengan@users.noreply.github.com>
2026-08-03 18:25:52 +0530

    Time: 123 ms (25.45%), Space: 15.8 MB (16.4%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        freq = [0] * 26 
+        for ch in s :
+            freq[ord(ch) - ord("a")] += 1 
+        for i in range(len(s)) :
+            if freq[ord(s[i]) - ord("a")] == 1 :
+                return i 
+        return -1
