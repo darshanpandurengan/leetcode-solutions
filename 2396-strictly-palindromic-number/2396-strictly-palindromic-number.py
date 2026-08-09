dec39fd497c196ae43aa54c9615976dea7a323b2
Darshan P <225718512+darshanpandurengan@users.noreply.github.com>
2026-08-09 21:47:54 +0530

    Time: 0 ms (100%), Space: 12.4 MB (66.32%) - LeetHub

---
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isStrictlyPalindromic(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        # Hint 
+        """
+        The number n in base (n - 2) is always 12, which is not palindromic.
+        """
+        return False
