8fb127a0772354bd534b49daa38e0f7013765baa
Darshan P <225718512+darshanpandurengan@users.noreply.github.com>
2026-08-10 18:27:49 +0530

    Time: 4 ms (2.88%), Space: 12.3 MB (55.04%) - LeetHub

--- 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.py
+++ 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.py
@@ -4,9 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # Sorting Method
-        temp = list(nums)
-        temp.sort() 
-        if temp[-1] >= 2 * temp[-2] :
-            return nums.index(temp[-1])
+        # Finding largetest and second largest 
+        largest = float("-inf")
+        second_largest = float("-inf")
+        largest_index = -1
+        for idx , num in enumerate(nums) :
+            if num > largest :
+                second_largest = largest
+                largest = num 
+                largest_index = idx
+            elif num > second_largest :
+                second_largest = num 
+        if largest >= 2 * second_largest :
+            return largest_index 
         return -1
